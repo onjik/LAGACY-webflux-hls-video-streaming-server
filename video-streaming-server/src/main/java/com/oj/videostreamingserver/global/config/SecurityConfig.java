f66c891a08103e17edcac0d27a9d0cc00f1556e2
Onji Kim <xodks672@gmail.com>
2023-05-28 00:03:56 +0900

    [FIX] CORS setting

--- video-streaming-server/src/main/java/com/oj/videostreamingserver/global/config/SecurityConfig.java
+++ video-streaming-server/src/main/java/com/oj/videostreamingserver/global/config/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -12,7 +17,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain filterChain(ServerHttpSecurity security) {
-        return security.cors().disable()
+        return security.cors()
+                .and()
                 .authorizeExchange()
                 .anyExchange().permitAll()
                 .and()
@@ -20,5 +26,17 @@ public class SecurityConfig {
                 .formLogin().disable()
                 .csrf().disable()
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOriginPatterns(List.of("*"));
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
     }
 }
