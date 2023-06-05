3ba0e51d705d87b44b9d56568c93e2f143b9607e
Onji Kim <xodks672@gmail.com>
2023-06-05 11:05:33 +0900

    [INITIAL] project-base

---
+++ media-server/src/test/java/com/ojkim/mediaserver/MediaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojkim.mediaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
